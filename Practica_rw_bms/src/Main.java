46163c2cd95fecbc387e48dbe920da3a5e5ff33c
brian12102023 <brian.12102023@gmail.com>
2026-06-17 23:57:31 -0600

    Integracion de la escritura y correccion de validacion

--- Practica_rw_bms/src/Main.java
+++ Practica_rw_bms/src/Main.java
@@ -2,6 +2,7 @@ import javax.imageio.metadata.IIOMetadataFormatImpl;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 public class Main {
@@ -9,21 +10,22 @@ public class Main {
         // Lectura y escritura de archivos con Java
         //Debe contener las diagonales dobles
         String ruta = "C:\\Users\\ThinkPad\\Desktop\\git\\numeros.txt";
-
+        String ruta2 = "C:\\Users\\ThinkPad\\Desktop\\git\\numeros2.txt"; // path o ruta hacia el archivo
         File file = new File(ruta); // Representación del archivo en memoria.
+        File escritura = new File(ruta2); // Representación del archivo en memoria.
         String linea = ""; // temporal, guardar 1 línea del archivo a la vez
         String texto = "";           // Variable para contener la fecha en letras
         String [] datos;        // Variable para extraer los datos de forma individual
         String [] resul = new String[3];    //Array para almacenar numeros como letras
         char caracter;
         int contador;
-
+        char resultado;
 
         String[] dias = {
                 "Uno", "Dos", "Tres", "Cuatro", "Cinco", "Seis", "Siete", "Ocho", "Nueve", "Diez",
                 "Once", "Doce", "Trece", "Catorce", "Quince", "Dieciséis", "Diecisiete", "Dieciocho",
                 "Diecinueve", "Veinte", "Veintiuno", "Veintidós", "Veintitrés", "Veinticuatro",
-                "Veinticinco", "Veintiséis", "Veintissiete", "Veintiocho", "Veintinueve", "Treinta",
+                "Veinticinco", "Veintiséis", "Veintisiete", "Veintiocho", "Veintinueve", "Treinta",
                 "Treinta y uno"
         };
 
@@ -43,20 +45,11 @@ public class Main {
         };
 
         String abecedario = "abcdefghijklmnopqrstuvwxyz";
-//        String[] abecedario = {
-//                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
-//                "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T",
-//                "U", "V", "W", "X", "Y", "Z"
-//        };
-
-//        char[] abecedario = {
-//                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-//                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
-//        };
 
         try{
             FileReader fr = new FileReader(file); // Cargar una copia del contenido (caché)
             BufferedReader br = new BufferedReader(fr); // Leer el caché
+            FileWriter fw = new FileWriter(escritura);
             while ((linea=br.readLine()) != null) { //lectura linea por línea
                 //Se declaro una variable para el array
                 datos = linea.split("/");//Extrae los datos individuales  y los guarda en un array
@@ -67,33 +60,43 @@ public class Main {
                 //Delimitacion del texto
                 texto = resul[0]+" de "+resul[1]+" de "+datos[2];
 
-
-                //Codificacion de la fecha
-                for (int i = 0; i < 1; i++) {
-                    caracter= texto.toLowerCase().charAt(i);
-                    System.out.println(caracter);
-                   if(abecedario.indexOf(caracter)>=0){     //Si es la letra
-                       contador = abecedario.indexOf(caracter);
-                       contador++;
-                       System.out.println(contador);
-
-                   }
-                }
-
                 //Impresion de los resultados
-//                System.out.print(datos[0]+datos[1]+datos[2]+" --> ");
-//                System.out.println(texto+"  -->  ");
+                System.out.print(datos[0]+datos[1]+datos[2]+" --> ");
+                fw.write(datos[0]+datos[1]+datos[2]+" --> ");
+                System.out.print(texto+"  -->  ");
+                fw.write(texto+"  -->  ");
 
-
-
+                //Codificacion
+                for (int i = 0; i < (texto.length()-4); i++) {
+                    caracter = texto.toLowerCase().charAt(i);
+                    if (caracter == 'z') {
+                        resultado= 'A';; // Si es 'z', imprimimos directamente 'a'
+                    } else if (caracter == ' ') {
+                        resultado= '-'; // Si es un espacio, imprimimos directamente '-'
+                    } else if (abecedario.indexOf(caracter) >= 0) {
+                        // Si la letra existe en el abecedario, avanzamos una posición
+                        contador = abecedario.indexOf(caracter);
+                        contador++;
+                        resultado = abecedario.toUpperCase().charAt(contador);
+                    } else {
+                        // Por si hay números, eñes o caracteres especiales
+                        resultado = caracter;
+                    }
+                    System.out.print(resultado);
+                    fw.write(resultado);
+                }
+                contador =0;
+                for (int i = texto.length()-4; i < texto.length(); i++) {
+                    caracter = texto.charAt(i);
+                    contador += Character.getNumericValue(caracter);
+                }
+                System.out.println(contador);
+                fw.write(contador+"\n");
             } //Fin del while
-//            fw.close();//guarda y cierra el flujo de datos hacia el archivo
-
+            fw.close();//guarda y cierra el flujo de datos hacia el archivo
         }catch(Exception ex){
             ex.printStackTrace(); //Indica cuales son los errores sin parar el programa
             System.out.println(ex.getMessage());//Excribir cual es el error
         }
-
-
     }
 }

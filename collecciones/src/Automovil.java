7223d5b07ef8738e313cbee8bc8a4c169096d36e
erik <21cs0190@cdserdan.tecnm.mx>
2026-06-23 12:38:49 -0600

    colecciones_eardb

---
+++ collecciones/src/Automovil.java
@@ -0,0 +1,88 @@
+public class Automovil {
+    private String marca;
+    private String modelo;
+    private int año;
+    private double precio;
+    private String tipo;
+    private String color;
+    private String transmision;
+
+    public Automovil(String marca, String modelo, int año, double precio, String tipo, String color, String transmision) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.año = año;
+        this.precio = precio;
+        this.tipo = tipo;
+        this.color = color;
+        this.transmision = transmision;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getTransmision() {
+        return transmision;
+    }
+
+    public void setTransmision(String transmision) {
+        this.transmision = transmision;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" +
+                "marca='" + marca + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", año=" + año +
+                ", precio=" + precio +
+                ", tipo='" + tipo + '\'' +
+                ", color='" + color + '\'' +
+                ", transmision='" + transmision + '\'' +
+                '}';
+    }
+}
---
+++ collecciones/src/Personas.java
@@ -0,0 +1,67 @@
+public class Personas {
+
+    private String nombre;
+    private String apellidoPat;
+    private String apellidoMat;
+    private String fechaNac;
+    private String lugarOrigen;
+
+    public Personas(String nombre, String apellidoPat, String apellidoMat, String fechaNac, String lugarOrigen) {
+        this.nombre = nombre;
+        this.apellidoPat = apellidoPat;
+        this.apellidoMat = apellidoMat;
+        this.fechaNac = fechaNac;
+        this.lugarOrigen = lugarOrigen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPat() {
+        return apellidoPat;
+    }
+
+    public void setApellidoPat(String apellidoPat) {
+        this.apellidoPat = apellidoPat;
+    }
+
+    public String getApellidoMat() {
+        return apellidoMat;
+    }
+
+    public void setApellidoMat(String apellidoMat) {
+        this.apellidoMat = apellidoMat;
+    }
+
+    public String getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(String fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getLugarOrigen() {
+        return lugarOrigen;
+    }
+
+    public void setLugarOrigen(String lugarOrigen) {
+        this.lugarOrigen = lugarOrigen;
+    }
+
+    @Override
+    public String toString() {
+        return "Personas{" +
+                "nombre='" + nombre + '\'' +
+                ", apellidoPat='" + apellidoPat + '\'' +
+                ", apellidoMat='" + apellidoMat + '\'' +
+                ", fechaNac='" + fechaNac + '\'' +
+                ", lugarOrigen='" + lugarOrigen + '\'' +
+                '}';
+    }
+}
